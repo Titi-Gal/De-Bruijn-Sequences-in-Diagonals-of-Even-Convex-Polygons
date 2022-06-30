3887e19a3f792a1c5fbc754f782543925ae7d549
ThiagoLRG <54580798+ThiagoLRG@users.noreply.github.com>
2022-06-30 10:04:08 -0300

    estruturando classe polígonos convexos pares

--- diagonals.py
+++ diagonals.py
@@ -2,51 +2,102 @@ from math import gcd
 from random import choice
 from time import perf_counter
 
-def Diagonais(vertices):
-    """Gera um conjunto com todas as diagonais de um polígono.
+"""
+Diagonais  = {0:[]}
+DiagomaisEmCiclo =  {2:{0:[], 1:[]},3:{...}}
+DiagonaisGrafo = [[][][]]
 
-    Essa função é eficiente, não gera nenhuma possibilidade a mais
-    """
-    diagonais = set()
-    for fim in range(2, vertices - 1):
-        diagonais.add(frozenset([0 , fim]))
-    for inicio in range(1, vertices - 2):
-        for fim in range(inicio + 2, vertices):
-            diagonais.add(frozenset([inicio, fim]))
-    return diagonais
+Diagonais_ComRepeticoes = {fim: [], fim: []}
+DiagonaisEmCiclo_ComRepeticoes = ?
+DiagonaisGrafo_ComRepeticoes = [[][][]]
+"""
 
-def DiagonaisEmCiclos(quantidadeVertices):
-    """Gera todas as diagonais por ciclos, cada ciclo tem um intervalo diferente;
+class EvenConvexPolygon:
+    def __init__(self, NumberOfVertices):
+        if NumberOfVertices % 2 != 0:
+            raise Exception("Number of vertices must be even")
+        self.NumberOfVertices = NumberOfVertices
+        self.Diagonals = self.__Diagonals()
+        self.DiagonalsInCycles = self.__DiagonalsInCycles()
+        self.DiagonalsGraph = self.__DiagonalsGraph()
+        self.DiagonalsRepeated = self.__DiagonalsRepeated()
 
-    Ex: {2: {0: [0, 2, 4, 0], 1: [1, 3, 5, 1]}, 3: {0: [0, 3], 1: [1, 4], 2: [2, 5]}}
-    """
-    diagonais = {}
-    #são necessarios ciclos de dois até diagonal divido por dois
-    for gerador in range(2, int(quantidadeVertices / 2) + 1):
-        diagonais.setdefault(gerador, {})
-        #cada ciclo vai aparecer mdc de vertices e gerador vezes, cada vez começando de inicio mais um
-        for rotacao in range(gcd(quantidadeVertices, gerador)):
-            ciclo = []
-            #incide de cada numero no ciclo
-            for indiceciclo in range(0 , int(quantidadeVertices / gcd(quantidadeVertices, gerador))):
-                #operacao para ter o intervalo certo entre os numeros
-                ciclo.append((rotacao + (indiceciclo * gerador)) % quantidadeVertices)
-            if len(ciclo) > 2:
-                #qualquer ciclo maior que dois vai terminar onde comecou
-                #tamanho dois voltar para o inicio seria uma repetição da diagonal
-                ciclo.append(rotacao)
-            diagonais[gerador].setdefault(rotacao, ciclo)
-    return diagonais
+    def __Diagonals(self):
+        """Gera um conjunto com todas as diagonais de um polígono.
 
-def GrafoVerticesEDiagonais(quantidadeVertices):
-    """Gera um grafo mostrando para cada vertices com quais outros vertices ele pode ser ligado para formar uma diagonal."""
-    grafo = []
-    for vertice in range(quantidadeVertices):
-        diagonaisDoVertices = []
-        for diagonal in range(vertice + 2, vertice + 2 + quantidadeVertices - 3):
-            diagonaisDoVertices += [diagonal % quantidadeVertices]
-        grafo += [diagonaisDoVertices]
-    return grafo
+        Essa função é eficiente, não gera nenhuma possibilidade a mais
+        """
+        diagonals = []
+        for fim in range(2, self.NumberOfVertices - 1):
+            diagonals.append([0 , fim])
+        for inicio in range(1, self.NumberOfVertices - 2):
+            for fim in range(inicio + 2, self.NumberOfVertices):
+                diagonals.append([inicio, fim])
+        return diagonals
+
+    def __DiagonalsInCycles(self):
+        """Gera todas as diagonais por ciclos, cada ciclo tem um intervalo diferente;
+
+        Ex: {2: {0: [0, 2, 4, 0], 1: [1, 3, 5, 1]}, 3: {0: [0, 3], 1: [1, 4], 2: [2, 5]}}
+        """
+        diagonais = {}
+        #são necessarios ciclos de dois até diagonal divido por dois
+        for gerador in range(2, int(self.NumberOfVertices / 2) + 1):
+            diagonais.setdefault(gerador, [])
+            #cada ciclo vai aparecer mdc de vertices e gerador vezes, cada vez começando de inicio mais um
+            for rotacao in range(gcd(self.NumberOfVertices, gerador)):
+                ciclo = []
+                #incide de cada numero no ciclo
+                for indiceciclo in range(0 , int(self.NumberOfVertices / gcd(self.NumberOfVertices, gerador))):
+                    #operacao para ter o intervalo certo entre os numeros
+                    ciclo.append((rotacao + (indiceciclo * gerador)) % self.NumberOfVertices)
+                if len(ciclo) > 2:
+                    #qualquer ciclo maior que dois vai terminar onde comecou
+                    #tamanho dois voltar para o inicio seria uma repetição da diagonal
+                    ciclo.append(rotacao)
+                diagonais[gerador].append(ciclo)
+        return diagonais
+
+    def __DiagonalsGraph(self):
+        """Gera um grafo mostrando para cada vertices com quais outros vertices ele pode ser ligado para formar uma diagonal."""
+        grafo = []
+        for vertice in range(self.NumberOfVertices):
+            diagonaisDoVertices = []
+            for diagonal in range(vertice + 2, vertice + 2 + self.NumberOfVertices - 3):
+                diagonaisDoVertices += [diagonal % self.NumberOfVertices]
+            grafo += [diagonaisDoVertices]
+        return grafo
+    
+    def __DiagonalsRepeated(self):
+        """Gera todas as possibilidades de repetir diagonais em grafos pares para que tenham caminhos eulerianos.
+
+        Os grafos pares precisam de (n-2)/2 diagonais repetidas para terem caminhos eulerianos 
+        Assume inicio no vertice 0 e muda o fim até n/2 para cada fim gera os grafos com as diagonais repetidas
+        """
+
+        def EncontraRepeticoes(verticeslistados, MontandoDiagonaisRepetidas, DiagonaisRepetidas):
+            """Encontra recursivamente as repetições nas listas sem os vértices já utilizados.
+            """
+            for vertice in verticeslistados[1:]: #para cada vertices do segundo em diante
+                if verticeslistados[0] + 1 != vertice: #se não for adjacente com o primeiro
+                    MontandoDiagonaisRepetidas.append((verticeslistados[0], vertice)) #acrescenta na lista montando
+                    #repassa a lista sem os dois vertices para a funcao
+                    EncontraRepeticoes([i for i in verticeslistados if i != verticeslistados[0] and i != vertice], MontandoDiagonaisRepetidas, DiagonaisRepetidas)
+                    #se lista montando tiver (tamanho n -2) / 2
+                    if len(MontandoDiagonaisRepetidas) == (self.NumberOfVertices - 2) / 2:
+                        #acrescenta nas diagonais repetidas prontas
+                        DiagonaisRepetidas.append(MontandoDiagonaisRepetidas.copy())
+                    #retira da lista montando a última possibilidade adicionada
+                    MontandoDiagonaisRepetidas.pop()
+            return DiagonaisRepetidas
+
+        finsErepeticoes = {} #dicionario onde serão guardados todas as repetições para cada final
+        for fim in range(1, int(self.NumberOfVertices / 2) + 1): #final até metade da quantidade de vértices
+            #gera uma lista com os vertices restantes e passa para encontrar repeticoes
+            DiagonaisRepetidas = EncontraRepeticoes([i for i in range(self.NumberOfVertices) if i != 0 and i != fim], [], [])
+            #quarda no dicionario
+            finsErepeticoes.update({fim: DiagonaisRepetidas})
+        return finsErepeticoes
 
 def GrafoCaminhosAleatorios(graph, diagonais, depth, depthcount=0, node=0, path=[0]):
     """Aleatoriamente encontra caminhos no grafo que passam por todas as diagonais pelo uma vez.
@@ -75,40 +126,13 @@ def GrafoCaminhosAleatorios(graph, diagonais, depth, depthcount=0, node=0, path=
     depthcount += 1
     return GrafoCaminhosAleatorios(graph, diagonais, depth, depthcount=depthcount, node=node, path=path)
 
-def DiagonaisRepetidasGrafosPares(vertices):
-    """Gera todas as possibilidades de repetir diagonais em grafos pares para que tenham caminhos eulerianos.
-
-    Os grafos pares precisam de (n-2)/2 diagonais repetidas para terem caminhos eulerianos 
-    Assume inicio no vertice 0 e muda o fim até n/2 para cada fim gera os grafos com as diagonais repetidas
-    """
-
-    def EncontraRepeticoes(verticeslistados, MontandoDiagonaisRepetidas=[], DiagonaisRepetidas=[]):
-        """Encontra recursivamente as repetições nas listas sem os vértices já utilizados.
-        """
-        for vertice in verticeslistados[1:]: #para cada vertices do segundo em diante
-            if verticeslistados[0] + 1 != vertice: #se não for adjacente com o primeiro
-                MontandoDiagonaisRepetidas.append((verticeslistados[0], vertice)) #acrescenta na lista montando
-                #repassa a lista sem os dois vertices para a funcao
-                EncontraRepeticoes([i for i in verticeslistados if i != verticeslistados[0] and i != vertice], MontandoDiagonaisRepetidas=MontandoDiagonaisRepetidas, DiagonaisRepetidas=DiagonaisRepetidas)
-                #se lista montando tiver (tamanho n -2) / 2
-                if len(MontandoDiagonaisRepetidas) == (vertices - 2) / 2:
-                    #acrescenta nas diagonais repetidas prontas
-                    DiagonaisRepetidas.append(MontandoDiagonaisRepetidas.copy())
-                #retira da lista montando a última possibilidade adicionada
-                MontandoDiagonaisRepetidas.pop()
-        return DiagonaisRepetidas
-
-    finsErepeticoes = {} #dicionario onde serão guardados todas as repetições para cada final
-    for fim in range(1, int(vertices / 2) + 1): #final até metade da quantidade de vértices
-        #gera uma lista com os vertices restantes e passa para encontrar repeticoes
-        DiagonaisRepetidas = EncontraRepeticoes([i for i in range(vertices) if i != 0 and i != fim])
-        #quarda no dicionario
-        finsErepeticoes.update({fim: DiagonaisRepetidas})
-    return finsErepeticoes
-
 #________________________________________________________________________________________________
 
-vertices = 18
+convexPolygon = EvenConvexPolygon(8)
+
+
+"""
+vertices = 1
 depth = 50
 maxfails = 5000000
 diagonais = Diagonais(vertices)
@@ -157,3 +181,4 @@ if len(pathsfound) != 0:
                 s += f'{num}, '
             f.write(s.rstrip(', ') + '\n')
             s = ''
+"""
